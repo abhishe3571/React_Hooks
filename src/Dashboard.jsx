4d859621bf64635f77d92f7f13606dabe8b1c125
Abhishek Singh <abhishek3571@gmail.com>
2024-08-13 14:39:10 +0530

    react servicess

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext, useState, useCallback } from "react";
 import { UserContext } from "./UserContext";
 import Order from "./Order";
-import { OrdersService } from "./Sevice";
+import { OrdersService, ProductsService } from "./Service";
 
 function Dashboard() {
   let [orders, setOrders] = useState([]);
@@ -9,48 +9,56 @@ function Dashboard() {
   //get context
   let userContext = useContext(UserContext);
 
+  //loadDataFromDatabase function that fetches data from 'orders' array from json file
+  let loadDataFromDatabase = useCallback(async () => {
+    //load data from database
+    let ordersResponse = await fetch(
+      `http://localhost:5000/orders?userid=${userContext.user.currentUserId}`,
+      { method: "GET" }
+    );
+
+    if (ordersResponse.ok) {
+      //status code is 200
+      let ordersResponseBody = await ordersResponse.json();
+
+      //get all data from products
+      let productsResponse = await ProductsService.fetchProducts();
+      if (productsResponse.ok) {
+        let productsResponseBody = await productsResponse.json();
+
+        //read all orders data
+        ordersResponseBody.forEach((order) => {
+          order.product = ProductsService.getProductByProductId(
+            productsResponseBody,
+            order.productId
+          );
+        });
+
+        console.log(ordersResponseBody);
+
+        setOrders(ordersResponseBody);
+      }
+    }
+  }, [userContext.user.currentUserId]);
+
   //executes only once - on initial render =  componentDidMount
   useEffect(() => {
     document.title = "Dashboard - eCommerce";
 
-    //load data from database
-    (async () => {
-      let ordersResponse = await fetch(
-        `http://localhost:5000/orders?userid=${userContext.user.currentUserId}`,
-        { method: "GET" }
-      );
-
-      if (ordersResponse.ok) {
-        //status code is 200
-        let ordersResponseBody = await ordersResponse.json();
-
-        //get all data from products
-        let productsResponse = await fetch("http://localhost:5000/products", {
-          method: "GET",
-        });
-        if (productsResponse.ok) {
-          let productsResponseBody = await productsResponse.json();
-
-          //read all orders data
-          ordersResponseBody.forEach((order) => {
-            order.product = productsResponseBody.find(
-              (prod) => prod.id === order.productId
-            );
-          });
-
-          console.log(ordersResponseBody);
-
-          setOrders(ordersResponseBody);
-        }
-      }
-    })();
-  }, [userContext.user.currentUserId]);
+    loadDataFromDatabase();
+  }, [userContext.user.currentUserId, loadDataFromDatabase]);
 
   return (
     <div className="row">
       <div className="col-12 py-3 header">
         <h4>
-          <i className="fa fa-dashboard"></i> Dashboard
+          <i className="fa fa-dashboard"></i> Dashboard{" "}
+          <button
+            className="btn btn-sm btn-info"
+            onClick={loadDataFromDatabase}
+          >
+            <i className="fa fa-refresh"></i> Refresh
+          </button>
         </h4>
       </div>
 
--- src/Sevice.js
+++
@@ -1,9 +0,0 @@
-export const OrdersService = {
-  getPreviousOrders: (orders) => {
-    return orders.filter((ord) => ord.isPaymentCompleted === true);
-  },
-
-  getCart: (orders) => {
-    return orders.filter((ord) => ord.isPaymentCompleted === false);
-  },
-};
